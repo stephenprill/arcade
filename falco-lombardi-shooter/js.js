d9280032d88376f021d38a0821f5c0cd18b8e14f
Stephen Prill <stephen.prill@gmail.com>
2015-03-16 01:26:58 -0600

    added falco-lombardi-shooter

---
+++ falco-lombardi-shooter/js.js
@@ -0,0 +1,26 @@
+var game = new Phaser.Game(800,600, Phaser.AUTO, 'phaser-demo', {preload: preload, create: create, update: update, render: render});
+
+var player;
+var starfield;
+
+function preload() {
+    game.load.image('starfield', '/assets/starfield.png');
+    game.load.image('ship', '/assets/player.png');
+}
+
+function create() {
+    //  The scrolling starfield background
+    starfield = game.add.tileSprite(0, 0, 800, 600, 'starfield');
+
+    //  The hero!
+    player = game.add.sprite(400, 500, 'ship');
+    player.anchor.setTo(0.5, 0.5);
+}
+
+function update() {
+
+}
+
+function render() {
+
+}
